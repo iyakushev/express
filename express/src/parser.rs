7816f26cf32accd213957e865a650bb35a4b14bd
Ivan Yakushev <iyakushev@quantumbrains.com>
2022-04-29 19:30:16 +0300

    [#3] Draft nom impl

--- express/src/parser.rs
+++ express/src/parser.rs
@@ -1,13 +1,44 @@
-use std::str::FromStr;
+use nom::{
+    bytes::complete::{tag, take_while_m_n},
+    character::complete::one_of,
+    sequence::tuple,
+    IResult,
+};
 
-use crate::lex::{Lexer, Token};
-
-struct Parser<'p> {
-    source: &'p str,
-    current_tok: Token,
-    token_it: Lexer<'p>,
+#[derive(Debug, PartialEq, Clone)]
+pub enum Literal {
+    Ident(String),
+    Number(f64),
 }
 
-pub trait Express {
-    fn parse(&self) -> AST;
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum Operation {
+    Plus,
+    Minus,
+    Times,
+    Divide,
+    Power,
+    Factorial,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum Expression {
+    Const(Literal),
+    Function(Vec<Expression>),
+    BinOp(Box<Expression>, Box<Expression>, Operation),
+    UnOp(Box<Expression>, Operation),
+}
+
+/// Parses operation token: `+, -, *, /, **, !`
+fn parse_op(input: &str) -> IResult<&str, Operation> {
+    let (inp, tok) = one_of("+-*/!")(input)?;
+    let tok = match tok {
+        '+' => Operation::Plus,
+        '-' => Operation::Minus,
+        '/' => Operation::Divide,
+        '*' => Operation::Times,
+        '!' => Operation::Factorial,
+        _ => unreachable!(),
+    };
+    Ok((inp, tok))
 }
