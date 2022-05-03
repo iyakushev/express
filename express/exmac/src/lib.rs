dfc99188a0f5a596d67671a8168ec92eb2821354
Nyanguy <iamaero7@gmail.com>
2022-05-03 17:05:20 +0300

    [#4] Impl runtime_callable macros

--- express/exmac/src/lib.rs
+++ express/exmac/src/lib.rs
@@ -1,7 +1,8 @@
 use proc_macro::TokenStream;
 use quote::{format_ident, quote};
-use syn::{spanned::Spanned, FnArg, Pat};
+use syn::{spanned::Spanned, FnArg, Pat, ReturnType};
 use types::{Callable, Type};
+
 /// This is a special macro that qualifies given function
 /// as a runtime acceptable. Note that the function can't
 /// have any internal mutable state.
@@ -13,39 +14,59 @@ use types::{Callable, Type};
 /// ```
 #[proc_macro_attribute]
 pub fn runtime_callable(_attr: TokenStream, item: TokenStream) -> TokenStream {
-    let function: syn::ItemFn = syn::parse(item).expect("Failed to parse input token stream");
+    let function: syn::ItemFn = syn::parse_macro_input!(item);
     let mut arguments: Vec<_> = Vec::new();
     for (idx, arg) in function.sig.inputs.clone().into_iter().enumerate() {
-        if let FnArg::Typed(t) = arg {
+        if let FnArg::Typed(t) = arg.clone() {
             if let Pat::Ident(id) = *t.pat.clone() {
+                let tp = t.ty;
                 let q = quote! {
-                let #id: #t.ty = unsafe { args.get_unchecked(#idx).into();
-                }; };
+                    let #id : #tp = unsafe { args.get_unchecked(#idx).into(); };
+                };
                 arguments.push(q);
-            };
-            syn::Error::new(
-                function.span(),
-                "This macro expects identifier as the argument name",
+            } else {
+                return syn::Error::new(
+                    t.span(),
+                    format!(
+                        "This macro expects identifier as an argument name. Got: {:?}",
+                        arg
+                    ),
+                )
+                .to_compile_error()
+                .into();
+            }
+        } else {
+            return syn::Error::new(
+                arg.span(),
+                "This macro cannot be applied to functions that use Reciever types like `self`",
             )
-            .to_compile_error();
+            .to_compile_error()
+            .into();
         }
-        syn::Error::new(
-            function.span(),
-            "This macro cannot be applied to functions that use `self`",
-        )
-        .to_compile_error();
     }
     let fn_name = format_ident!("_{}", function.sig.ident);
+    if let ReturnType::Default = function.sig.output {
+        return syn::Error::new(
+            function.sig.output.span(),
+            "Function must be explicit in its return type",
+        )
+        .to_compile_error()
+        .into();
+    }
+
     let stmts = function.block.stmts;
-    let callable = quote! {
+    quote! {
+        #[allow(non_camel_case_types)]
         struct #fn_name;
 
         impl Callable for #fn_name {
             fn call(&self, args: &[Type]) -> Type {
                 #( #arguments )*
-                #( #stmts )*
+                return {
+                    #( #stmts )*
+                }.into();
             }
         }
-    };
-    callable.into()
+    }
+    .into()
 }
--- express/exmac/tests/integration_test.rs
+++ express/exmac/tests/integration_test.rs
@@ -2,6 +2,6 @@ use exmac::runtime_callable;
 use types::{Callable, Type};
 
 #[runtime_callable]
-fn Foo(input: f64) -> f64 {
+fn foo(input: f64) -> f64 {
     input * 2.0 + 2.0
 }
