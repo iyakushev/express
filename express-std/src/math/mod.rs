43a966ff0381f591f504f55f9271d0e0282f9c18
Ivan Yakushev <iyakushev@quantumbrains.com>
2022-05-11 16:01:33 +0300

    Add math constants

--- express-std/src/math/mod.rs
+++ express-std/src/math/mod.rs
@@ -1,1 +1,6 @@
-mod log;
+pub mod log;
+
+pub const EPS: f64 = std::f64::consts::E;
+pub const PI: f64 = std::f64::consts::PI;
+pub const TAU: f64 = std::f64::consts::TAU;
+pub const LN_2: f64 = std::f64::consts::LN_2;
